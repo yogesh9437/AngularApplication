c6de6ac5fcb9992c33b5ef7031402b845c7716ed
YOGESH PRASAD SATAPATHY <38222524+yogesh9437@users.noreply.github.com>
2022-04-23 01:36:14 +0530

    API Project added
    API Project set up done

---
+++ API/PaymentAPI/Migrations/20220422195347_Initial Setup.cs
@@ -0,0 +1,52 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace PaymentAPI.Migrations
+{
+    public partial class InitialSetup : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "PaymentDetails",
+                columns: table => new
+                {
+                    PaymentDetailId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    CardOwnerName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    CardNumber = table.Column<string>(type: "nvarchar(16)", nullable: true),
+                    ExpirationDate = table.Column<string>(type: "nvarchar(5)", nullable: true),
+                    SecurityCode = table.Column<string>(type: "nvarchar(3)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PaymentDetails", x => x.PaymentDetailId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "UserDetails",
+                columns: table => new
+                {
+                    UserId = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    UserName = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    MailId = table.Column<string>(type: "nvarchar(16)", nullable: true),
+                    Password = table.Column<string>(type: "nvarchar(8)", nullable: true),
+                    Address = table.Column<string>(type: "nvarchar(100)", nullable: true),
+                    UserRole = table.Column<string>(type: "nvarchar(10)", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_UserDetails", x => x.UserId);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "PaymentDetails");
+
+            migrationBuilder.DropTable(
+                name: "UserDetails");
+        }
+    }
+}
---
+++ API/PaymentAPI/Models/UserDetails.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace PaymentAPI.Models
+{
+    public class UserDetails
+    {
+        [Key]
+        public int UserId { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string UserName { get; set; }
+
+        [Column(TypeName = "nvarchar(16)")]
+        public string MailId { get; set; }
+
+        [Column(TypeName = "nvarchar(8)")]
+        public string Password { get; set; }
+
+        [Column(TypeName = "nvarchar(100)")]
+        public string Address { get; set; }
+
+        [Column(TypeName = "nvarchar(10)")]
+        public string UserRole { get; set; }
+    }
+}
